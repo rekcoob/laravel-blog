f407919d412d36ff8e65cf874e2190ddfd9420a8
ImDakoty <shanoun@webpack.sk>
2020-03-08 18:05:57 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/hello', function () {
+    //return view('welcome');
+    return '<h1>Hello World</h1>';
+});
+*/
+/*
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user' . $id. 'Named ' . $name;
+});
+*/
+
+Route::get('/', 'PagesController@index');
+
+/* ---------LOADING DIRECTLY-----------------
+Route::get('/about', function () {
+    return view('pages.about');
+});
+*/
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
